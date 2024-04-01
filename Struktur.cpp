274aa364edbacd66b533df5c72b024df5c2c303f
Lenovo <9.3razzinayyuri@gmail.com>
2024-04-01 18:29:05 +0700

    done

--- Struktur.cpp
+++ Struktur.cpp
@@ -1,20 +1,28 @@
-// Struktur.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
